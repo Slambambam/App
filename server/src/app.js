230b56f2ebbc8830885c9844523bc6c2c02b7879
Slambambam <7293709+Slambambam@users.noreply.github.com>
2019-02-14 03:36:17 +0200

    i

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,15 @@
-console.log('hello')
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (reg, res) => {
+app.post('/regiter', (req, res) => {
     res.send({
-      message: 'hello Api'
+      message: 'Hello Your fist call back  Api user'
     })
 })
 
