73ca6753512282765e34f6e8e6de6e8d2b56d544
Slambambam <7293709+Slambambam@users.noreply.github.com>
2019-02-15 22:16:30 +0200

    port set

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
+
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
@@ -22,4 +25,9 @@ app.post('/login', (req, res) => {
   })
 })
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync(
+  then(() => {
+    app.listen(config.port) 
+    console.log(`Sever started on port ${config.port}`)  
+})
+)
